093e0dc23a7a0ef324de8f859f9c94bf490c07a4
Yingting Huang <huangyingting@outlook.com>
2024-12-20 20:12:55 +0800

    Remove unused output variables and update Key Vault URL reference in Terraform configuration

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -1,28 +1,3 @@
-output "azure_sql_server_fqdn" {
-  value       = module.azure_sql.server_fqdn
-  description = "Azure SQL server full qualified domain name"
-}
-
-output "azure_sql_database_name" {
-  value       = module.azure_sql.database_name
-  description = "Azure SQL server database name"
-}
-
-output "service_account_namespace" {
-  value       = local.namespace
-  description = "Namespace of the service account"
-}
-
-output "service_account_name" {
-  value       = local.serviceaccount
-  description = "Name of the service account"
-}
-
-output "user_assigned_identity_client_id" {
-  value       = module.user_assigned_identity.client_id
-  description = "The client ID of the user assigned identity"
-}
-
 output "kubectl_commands" {
   value = <<EOF
 kubectl create ns ${local.namespace}
@@ -52,7 +27,7 @@ spec:
       name: wkldid-java
       env:
       - name: KEYVAULT_URL
-        value: ${module.azure_keyvault.vault_url}
+        value: ${module.azure_keyvault.vault_uri}
       - name: KEYVAULT_SECRET_NAME
         value: ${module.azure_keyvault.secret_name}
       - name: SQL_SERVER_FQDN
@@ -80,7 +55,7 @@ spec:
       args: ["-c", "java -cp app.jar org.icsu.wkldid.KV"]
       env:
       - name: KEYVAULT_URL
-        value: ${module.azure_keyvault.vault_url}
+        value: ${module.azure_keyvault.vault_uri}
       - name: KEYVAULT_SECRET_NAME
         value: ${module.azure_keyvault.secret_name}
   nodeSelector:
